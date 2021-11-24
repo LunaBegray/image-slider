66958398489c75911d27d11e7b78e6a7bf0be7c2
luna <lunashoval@gmail.com>
2021-11-24 17:33:44 -0500

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const forBtn = document.querySelector(".forwardBtn");
 const bacBtn = document.querySelector(".backwardBtn");
 const photos = document.querySelector(".photos");
-let marginRight = 800;
+let marginLeft = 140;
+let useMarR;
 forBtn.addEventListener('click', function(){
     console.log("forward button clicked");
-    marginRight -= 200;
-    useMarR = marginRight.toString();
-    photos.style.marginRight = useMarR + "px";
-    console.log(photos.style.marginRight);
+    marginLeft -= 200;
+    if(marginLeft >= -460 ){
+        useMarR = marginLeft.toString();
+        photos.style.marginLeft = useMarR + "px";
+        console.log(photos.style.marginLeft);
+    }
+});
+bacBtn.addEventListener('click', function(){
+    console.log("backward button clicked");
+    marginLeft += 200;
+    if(marginLeft <= 140){
+         useMarR = marginLeft.toString();
+         photos.style.marginLeft = useMarR + "px";
+         console.log(photos.style.marginLeft);
+    }
 });
