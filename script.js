5df2c84cb876b34ea54881f8e52768d84bec2ac4
luna <lunashoval@gmail.com>
2021-11-24 18:25:01 -0500

    script.js

--- script.js
+++ script.js
@@ -3,6 +3,37 @@ const bacBtn = document.querySelector(".backwardBtn");
 const photos = document.querySelector(".photos");
 let marginLeft = 140;
 let useMarR;
+const dot1 = document.querySelector(".dot1");
+const dot2 = document.querySelector(".dot2");
+const dot3 = document.querySelector(".dot3");
+const dot4 = document.querySelector(".dot4");
+dot1.style.background = 'orange';
+function updateCircle(){
+  if(marginLeft == 140){
+       dot1.style.backgroundColor = 'orange'
+       dot2.style.backgroundColor = '#bbb'
+       dot3.style.backgroundColor = '#bbb'
+       dot4.style.backgroundColor = '#bbb'
+  }
+  if(marginLeft == -60){
+       dot1.style.backgroundColor = '#bbb'
+       dot2.style.backgroundColor = 'orange'
+       dot3.style.backgroundColor = '#bbb'
+       dot4.style.backgroundColor = '#bbb'
+  } 
+  if(marginLeft == -260){
+       dot1.style.backgroundColor = '#bbb'
+       dot2.style.backgroundColor = '#bbb'
+       dot3.style.backgroundColor = 'orange'
+       dot4.style.backgroundColor = '#bbb'
+  }
+  if(marginLeft == -460){
+       dot1.style.backgroundColor = '#bbb'
+       dot2.style.backgroundColor = '#bbb'
+       dot3.style.backgroundColor = '#bbb'
+       dot4.style.backgroundColor = 'orange'
+  }
+}
 forBtn.addEventListener('click', function(){
     console.log("forward button clicked");
     marginLeft -= 200;
@@ -10,6 +41,7 @@ forBtn.addEventListener('click', function(){
         useMarR = marginLeft.toString();
         photos.style.marginLeft = useMarR + "px";
         console.log(photos.style.marginLeft);
+        updateCircle();
     } else { marginLeft += 200}
 });
 bacBtn.addEventListener('click', function(){
@@ -19,5 +51,6 @@ bacBtn.addEventListener('click', function(){
          useMarR = marginLeft.toString();
          photos.style.marginLeft = useMarR + "px";
          console.log(photos.style.marginLeft);
+         updateCircle();
     } else {marginLeft -= 200}
 });
